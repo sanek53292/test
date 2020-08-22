2d6f18729b3031380bdbc654896714e18640d329
Alex <sanek53292@gmail.com>
2020-08-22 23:52:14 +0300

    Это наш первый комит

---
+++ src/js/script.js
@@ -0,0 +1,61 @@
+
+
+var leftBorderWidth = 1;
+
+const pi = 3.14;
+
+{
+	let second = 2;
+}
+
+var number = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string' * 9);
+
+var und;
+console.log(und);
+
+obj = {
+	name: "John",
+	age: 25,
+	isMarried: false
+}
+
+console.log(obj["name"]);
+
+var arr = ['plum', 'orange', 'apple'];
+console.log(arr[2]);
+
+// alert("Hello!");
+// confirm("Are You here?");
+
+// var answer = +prompt("Вам есть 18?", "18");
+
+// console.log(typeof(answer));
+// // console.log( 4 + ' - object');
+
+var incr = 10,
+	decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr++);
+// console.log(decr--);
+
+console.log(5%2);
+
+console.log('10' === 10);
+
+var isCheked = true;
+var isClose = true;
+
+console.log(isCheked || !isClose);
+
